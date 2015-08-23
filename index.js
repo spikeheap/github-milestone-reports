e540ebc72dc0b7b201a5027411669e793e179406
Ryan Brooks <ryanbrooksis@gmail.com>
2015-08-23 22:03:00 +0100

    refactoring for promises, and collecting output for end.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 require("dotenv").load();
 
+var markdown = require( "markdown" ).markdown;
 var GitHubApi = require("github");
 
 var github = new GitHubApi({
@@ -19,57 +20,88 @@ var repo = 'growkudos';
 var ignoredMilestones = ['Product Backlog', 'Support'];
 var PERCENTAGE_PRECISION = 0; // number of decimal places
 
-github.issues.getAllMilestones({user: "growkudos", repo: "growkudos"}, function (err, milestones) {
+//the output markdown, as an array of lines
+var outputMarkdown = [];
+outputMarkdown.push('# Sprint report');
+outputMarkdown.push('');
 
-  milestones.filter(milestoneFilter).forEach(function (milestone) {
-		github.issues.repoIssues({user: "growkudos", repo: "growkudos", milestone: milestone.number, state: 'all'}, function (err, issues) {
-			var milestoneStoryPoints = 0;
-			var milestoneStoryPointsClosed = 0;
-			var milestoneStoryPointsOpen = 0;
-			var unestimatedIssues = [];
-
-			console.log('# [' + milestone.title + '](' + milestone.html_url + ')');
-			console.log(milestone.description + '\n');
-
-			issues.forEach(function (issue) {
-				var issueLabels = issue.labels.filter(isNotAStoryPointLabel).map(extractName);
-
-				var issueStoryPoints = issue.labels
-					.filter(isAStoryPointLabel)
-					.map(extractStoryPointValue)
-					.reduce(addTogether, 0);
-
-				if(issueStoryPoints == 0){
-					unestimatedIssues.push(issue);
-				}else{
-					milestoneStoryPoints += issueStoryPoints;
-					if(issue.state === 'open'){
-						milestoneStoryPointsOpen += issueStoryPoints;
-					}else{
-						milestoneStoryPointsClosed += issueStoryPoints;
-					}
-				}
-
-				// wrap issue labels in parentheses
-				if(issueLabels.length > 0) {
-					issueLabels = ' (' + issueLabels.map(makePreformatted).join(',') + ')';
-				} else {
-					issueLabels = '';
-				}
-
-				console.log("* [#" + issue.number + "](" + issue.html_url + "): " + issue.title + issueLabels);
+new Promise(function(resolve){
+	github.issues.getAllMilestones({user: "growkudos", repo: "growkudos"}, function (err, milestones) {
+		if(err) { throw err; } 
+		resolve(milestones);
+	});
+}).then(function (milestones) {
+	var milestonePromises = milestones.filter(milestoneFilter).map(function (milestone) {
+		return new Promise(function (resolve) {
+			github.issues.repoIssues({user: "growkudos", repo: "growkudos", milestone: milestone.number, state: 'all'}, function (err, issues) {
+				if(err) { throw err; } 
+				milestone.issues = issues;
+				resolve(milestone);
 			});
+		});
+	});
+	return Promise.all(milestonePromises)
+}).then(function (milestones) {
+	milestones.forEach(function (milestone) {
+		var milestoneStoryPoints = 0;
+		var milestoneStoryPointsClosed = 0;
+		var milestoneStoryPointsOpen = 0;
+		var unestimatedIssues = [];
 
-			console.log('\n');
+		outputMarkdown.push('## [' + milestone.title + '](' + milestone.html_url + ')');
+		outputMarkdown.push(milestone.description);
+		outputMarkdown.push('');
 
-			console.log('#### Sprint summary');
-			console.log('* ' + issues.length + ' issues (' + milestone.open_issues + ' open, ' + milestone.closed_issues + ' closed, ' + parseFloat((milestone.closed_issues / issues.length) * 100).toFixed(PERCENTAGE_PRECISION) + '% complete).');
-			console.log('* ' + milestoneStoryPoints + ' story points estimated (' + milestoneStoryPointsOpen + ' points open, ' + milestoneStoryPointsClosed + ' points closed, ' + parseFloat((milestoneStoryPointsClosed / milestoneStoryPoints) * 100).toFixed(PERCENTAGE_PRECISION) + '% complete).');
-			console.log('* ' + unestimatedIssues.length + ' unestimated issues (' + unestimatedIssues.map(extractsIssueNumberWithLink).join(',')  + ').');
+		milestone.issues.forEach(function (issue) {
+			var issueLabels = issue.labels.filter(isNotAStoryPointLabel).map(extractName);
 
-			console.log('\n');
-		})
-  })
+			var issueStoryPoints = issue.labels
+				.filter(isAStoryPointLabel)
+				.map(extractStoryPointValue)
+				.reduce(addTogether, 0);
+
+			if(issueStoryPoints == 0){
+				unestimatedIssues.push(issue);
+			}else{
+				milestoneStoryPoints += issueStoryPoints;
+				if(issue.state === 'open'){
+					milestoneStoryPointsOpen += issueStoryPoints;
+				}else{
+					milestoneStoryPointsClosed += issueStoryPoints;
+				}
+			}
+
+			// wrap issue labels in parentheses
+			if(issueLabels.length > 0) {
+				issueLabels = ' (' + issueLabels.map(makePreformatted).join(', ') + ')';
+			} else {
+				issueLabels = '';
+			}
+
+			outputMarkdown.push("* [#" + issue.number + "](" + issue.html_url + "): " + issue.title + issueLabels);
+		});
+
+		outputMarkdown.push('');
+		outputMarkdown.push('#### Sprint summary');
+		outputMarkdown.push('* ' + milestone.issues.length + ' issues (' + milestone.open_issues + ' open, ' + milestone.closed_issues + ' closed, ' + parseFloat((milestone.closed_issues / milestone.issues.length) * 100).toFixed(PERCENTAGE_PRECISION) + '% complete).');
+		outputMarkdown.push('* ' + milestoneStoryPoints + ' story points estimated (' + milestoneStoryPointsOpen + ' points open, ' + milestoneStoryPointsClosed + ' points closed, ' + parseFloat((milestoneStoryPointsClosed / milestoneStoryPoints) * 100).toFixed(PERCENTAGE_PRECISION) + '% complete).');
+		outputMarkdown.push('* ' + unestimatedIssues.length + ' unestimated issues (' + unestimatedIssues.map(extractsIssueNumberWithLink).join(', ')  + ').');
+		outputMarkdown.push('');
+	});
+
+	return outputMarkdown.join('\n')
+}).then(function (markdownText) {
+	console.log('<!DOCTYPE html>');
+	console.log('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">');
+	console.log('<div class="container">');
+	console.log('<div class="row">');
+	console.log('<div class="col-xs-12">');
+	console.log(markdown.toHTML(markdownText));
+	console.log('</div>');
+	console.log('</div>');
+	console.log('</div>');
+}).catch(function (err) {
+	console.log('ERROR', err);
 });
 
 // Private functions
